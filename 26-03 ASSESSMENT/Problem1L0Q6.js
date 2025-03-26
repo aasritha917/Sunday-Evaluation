88aef1fc34191e93b691e7a7c67458457468c72f
Kaluvala Aasritha <aasrithaaishu@gmail.com>
2025-03-26 21:20:37 +0530

    21/3 Assessment

--- 26-03 ASSESSMENT/Problem1L0Q6.js
+++
@@ -1,6 +0,0 @@
-function Timer(duration,onComplete){
-    setTimeout(()=>{
-        onComplete(`Timer of ${duration} ms finished`);
-    }, duration);
-}
-Timer(2000, (message) => console.log(message));
